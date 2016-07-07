45be7469b1e7e35001a6eb5a5e521a4fc376f7b5
Frederique Petris <frederique.petris@gmail.com>
2016-07-07 21:29:39 +0200

    add logo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706163553) do
+ActiveRecord::Schema.define(version: 20160707192723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "logos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
