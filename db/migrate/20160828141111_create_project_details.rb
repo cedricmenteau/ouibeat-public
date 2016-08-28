16b3c7d0dfeaf21813e814dad16c9d4468b4c111
Frederique Petris <frederique.petris@gmail.com>
2016-08-28 19:25:04 +0200

    new model projectDetails

---
+++ db/migrate/20160828141111_create_project_details.rb
@@ -0,0 +1,11 @@
+class CreateProjectDetails < ActiveRecord::Migration
+  def change
+    create_table :project_details do |t|
+      t.string :title
+      t.text :text
+      t.references :project, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
