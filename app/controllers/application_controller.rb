072340660317f1370337b9ae5e6129fef52a6d68
cedricmenteau <me.cedric@gmail.com>
2016-09-18 12:49:20 +0200

    Setup meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def default_url_options
+    if Rails.env.production?
+      { host: 'ouibeat-public.herokuapp.com' }
+    else
+      { host: ENV['HOST'] || 'localhost:3000' }
+    end
+  end
+
 end
