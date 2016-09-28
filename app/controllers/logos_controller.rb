32ca85227c86dcce3dbd37217ac2fbfacf8d9002
Frederique Petris <frederique.petris@gmail.com>
2016-09-28 23:08:39 +0200

    remove button more when no more logos to display + display with slideDown

--- app/controllers/logos_controller.rb
+++ app/controllers/logos_controller.rb
@@ -2,7 +2,7 @@ class LogosController < ApplicationController
 
   def index
     @logos = Logo.where('position > ?', params[:position]).includes(:picture_files).limit(6)
-    if @logos[5].present? && params[:position].to_i < Logo.last.position
+    if @logos[5].present? && @logos[5].position < Logo.last.position
       params[:position] = @logos[5].position
     else
       params[:position] = nil
